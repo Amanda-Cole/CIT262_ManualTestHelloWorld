074ca7554d400c2f3ce351c15a6c00a84b3d3338
Amanda-Cole <col22031@byui.edu>
2023-10-17 20:03:57 -0600

    Initial commit

---
+++ test/helloworld.test.js
@@ -0,0 +1,21 @@
+// import sayHello from "../utils/helloworld.mjs";
+// import assert from 'assert';
+
+// console.log("Hello Amanda Printed");
+
+// it("Test Hello World", ()=>{
+//     const hello = sayHello();
+//     assert.equal(hello,"Hello Amanda");
+    
+// });
+
+function sayHello() {
+    const greeting = "hello";
+
+    console.log(greeting);
+    return greeting;
+  }
+  
+  // Call the function to demonstrate it
+  sayHello();
+
